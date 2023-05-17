a24b692a796473ac328f73cb2577af5e218b71d5
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-17 11:10:10 +0300

    + Spring Security Configuration (construct and use the InMemoryUserDetailsManager bean)

---
+++ src/main/java/com/mcfly/crud_rest_demo/security/DemoSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.mcfly.crud_rest_demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        final UserDetails john
+                = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        final UserDetails mary
+                = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE, MANAGER")
+                .build();
+
+        final UserDetails susan
+                = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE, MANAGER, ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+}
