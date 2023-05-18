a07e9223e711f8da6a11470183dbc9424b1c0130
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-18 14:17:38 +0300

    + JDBC Authentication

--- src/main/java/com/mcfly/crud_rest_demo/security/DemoSecurityConfig.java
+++ src/main/java/com/mcfly/crud_rest_demo/security/DemoSecurityConfig.java
@@ -4,14 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+        httpSecurity.httpBasic();               // Enable basic authentication.
+        httpSecurity.csrf().disable();          // There is no need to protect against csrf attacks for REST API (stateless).
+        return httpSecurity.build();
+    }
+
+    /* Hardcoded users.
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         final UserDetails john
@@ -36,18 +57,5 @@ public class DemoSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
-        httpSecurity.httpBasic();               // Enable basic authentication.
-        httpSecurity.csrf().disable();          // There is no need to protect against csrf attacks for REST API (stateless).
-        return httpSecurity.build();
-    }
+    */
 }
